01f4b8660b7f2f7a98d2b53270397d090c0f2a83
Alex Kuzmin <142922146+kac13404@users.noreply.github.com>
2024-03-30 20:20:16 +0500

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два
+неотрицательных числа m и n.
+
+Пример:
+
+m = 2, n = 3 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAkk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return FunctionAkk(m - 1, 1);
+    else return FunctionAkk(m - 1, FunctionAkk(m, n - 1));
+}
+
+Console.Write($"A(m,n) = {FunctionAkk(m, n)}");
---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать
+рекурсию, не использовать циклы.
+
+Пример:
+
+[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+*/
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+
+string PrintArray(int[] array)
+{
+    return string.Join(", ", array);
+}
+int[] ArraySwap(int[] array, int index)
+{
+
+    if (index <= 0)
+    {
+        return array;
+    }
+    int temp = array[index];
+    array[index] = array[array.Length - index - 1];
+    array[array.Length - index - 1] = temp;
+    return ArraySwap(array, index -= 2);
+}
+
+Console.WriteLine($"{PrintArray(ArraySwap(array, array.Length - 1))}");
