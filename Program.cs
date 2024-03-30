4007fdb99097e27845a083f038e563613369a7f0
Alex Kuzmin <142922146+kac13404@users.noreply.github.com>
2024-03-30 19:30:51 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Пример:
+
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 8; N = 4 -> "8, 7, 6, 5, 4"
+*/
+
+Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNaturalNumbers(int m, int n)
+
+{
+    if (m == 0 || n == 0) return;
+
+    if (m < n)
+    {
+        Console.Write($"{m}, ");
+        PrintNaturalNumbers(m + 1, n);
+        return;
+    }
+
+    else
+    {
+        if (m > n)
+        {
+            Console.Write($"{m}, ");
+            PrintNaturalNumbers(m - 1, n);
+            return;
+        }
+    }
+
+    if (m == n)
+    {
+        Console.Write($"{m}, ");
+        return;
+    }
+}
+
+PrintNaturalNumbers(m, n);
